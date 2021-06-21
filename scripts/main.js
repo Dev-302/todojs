1739d9d1e02fb8906df16f1c1978f23b823f06f1
Dev-302 <gebruiker302@outlook.com>
2021-06-21 20:43:52 +0200

    created 3 count lists

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+function updateCounters() {
+
+const totalCount = document.getElementById("total-count");
+const totalTodos = document.getElementsByClassName("todo").length;
+totalCount.innerHTML = totalTodos;
+
+const completedCount = document.getElementById("completed-count");
+const totalTodos = document.getElementsByClassName("todo").length;
+completedCount.innerHTML = completedTodos;
+
+const todoCount = document.getElementById("todo-count");
+const uncompletedTodos = totalTodos - completedTodos;
+completedCount.innerHTML = uncompletedTodos;
+}
+
+updateCounters();
