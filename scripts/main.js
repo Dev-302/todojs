edecc644d708814aeddb965069f1726c88f6dd8f
Dev-302 <gebruiker302@outlook.com>
2021-06-21 21:30:13 +0200

    fixed counter error

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,37 @@
 function updateCounters() {
+    // Total number of todos
     const totalCount = document.getElementById("total-count");
     const totalTodos = document.getElementsByClassName("todo").length;
     totalCount.innerHTML = totalTodos;
-
+  
+    // Total number of completed todos
     const completedCount = document.getElementById("completed-count");
     const completedTodos = document.getElementsByClassName("completed").length;
     completedCount.innerHTML = completedTodos;
-
+  
+    // Total number of uncompleted todos
     const todoCount = document.getElementById("todo-count");
     const uncompletedTodos = totalTodos - completedTodos;
     todoCount.innerHTML = uncompletedTodos;
-}
+  }
+  
+  updateCounters();
 
 function toggleDone(event) {
-    // get the checkbox from the event object
-    const checkbox = event.target;
-  
+    const checkbox = event.currentTarget;
+    // check the checked status of the checkbox
     if (checkbox.checked) {
-        // change the checkbox so that it shows up as completed
-        checkbox.parentElement.parentElement.className = "Todo completed";
+      // the "completed" class is set on the parent element, the <li>
+      checkbox.parentElement.parentElement.className = "todo completed";
     } else {
-        // change the checkbox so that it shows up as todo
-        checkbox.parentElement.parentElement.className = "Todo";
+      checkbox.parentElement.parentElement.className = "todo";
     }
-    // update the counters, now that we have updated the checkbox
+  
     updateCounters();
   }
   
-  // add a "change" event listener to every checkbox,
   const checkboxes = document.querySelectorAll(".todo input");
-  // and use the "toggleDone" function as the callback
-
+  
   for (let i = 0; i < checkboxes.length; i++) {
-      const element = checkboxes[i];
-      element.addEventListener("change", toggleDone);
+    checkboxes[i].addEventListener("change", toggleDone);
   }
-
-updateCounters();
